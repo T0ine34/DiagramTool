20a6635a1e85aca600e2e2b1edfaede4b337ea32
Antoine <91411073+T0ine34@users.noreply.github.com>
2024-10-08 14:40:45 +0200

    removing useless scripts

--- build.sh
+++
@@ -1,8 +0,0 @@
-# ensure an argument is passed
-if [ -z "$1" ]; then
-    echo "Usage: $0 <input file>"
-    exit 1
-fi
-
-python src/python/python.py $1 --dump --debug
-python src/svg/__main__.py
--- install.sh
+++
@@ -1,1 +0,0 @@
-apt-get install texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
